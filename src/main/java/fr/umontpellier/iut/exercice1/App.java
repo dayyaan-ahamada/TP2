e1ca1dc1f88b663bfe7c8f1b41ec13d96c437039
Dayyaan <dayyaan.ahamada@etu.umontpellier.fr>
2019-02-13 16:36:29 +0100

    commit

--- src/main/java/fr/umontpellier/iut/exercice1/App.java
+++ src/main/java/fr/umontpellier/iut/exercice1/App.java
@@ -3,7 +3,11 @@ package fr.umontpellier.iut.exercice1;
 public class App {
 
     public static void main(String[] args) {
-        throw new RuntimeException("Not yet implemented !");
+        FizzBuzz Fb = new FizzBuzz();
+        String[] result = Fb.computeList(5);
+        for (int i=0; i<20; i++) {
+            System.out.print(result[i] + ';');
+        }
     }
 
 }
--- src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
+++ src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
@@ -1,12 +1,29 @@
 package fr.umontpellier.iut.exercice1;
 
-public class FizzBuzz {
+public class FizzBuzz<s> {
 
     public String getValue(int i) {
-        throw new RuntimeException("Not yet implemented !");
+        String s;
+        if(i%5==0 && i%3==0) {
+            s="FizzBuzz";
+        }
+        else if(i%3==0){
+            s="Fizz";
+        }
+        else if(i%5==0){
+            s="Buzz";
+        }
+        else {
+            s=Integer.toString(i);
+        }
+        return s;
     }
 
     public String[] computeList(int i) {
-        throw new RuntimeException("Not yet implemented !");
+        String[] result = new String[i];
+        for (int j = 0; j < i; j++){
+            result[j] = getValue(j+1);
+        }
+        return result;
     }
 }
