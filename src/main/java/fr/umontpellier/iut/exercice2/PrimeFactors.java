7074d19763dd56a5aedfceee815c44c78448a294
Dayyaan <dayyaan.ahamada@etu.umontpellier.fr>
2019-02-13 16:38:04 +0100

    Yes

--- src/main/java/fr/umontpellier/iut/exercice2/PrimeFactors.java
+++ src/main/java/fr/umontpellier/iut/exercice2/PrimeFactors.java
@@ -1,9 +1,21 @@
 package fr.umontpellier.iut.exercice2;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class PrimeFactors {
     public List<Integer> computeFactors(int number) {
-        throw new RuntimeException("Not yet implemented");
+
+        List<Integer> list = new ArrayList<>();
+        int i=2;
+        while(number!=1) {
+            while(number%i!=0){
+                i++;
+            }
+            number=number/i;
+            list.add(i);
+            i=2;
+        }
+        return list;
     }
 }
